cc5d33c243551420065d4bee87de7dc25e439451
Juan Fox <foxj0875@gmail.com>
2019-01-15 13:01:52 -0500

    authentication flow with passportjs and cookie-session is complete

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('cookie-session');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -12,6 +14,14 @@ mongoose.connect(keys.mongoURI, () => {
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(session({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
