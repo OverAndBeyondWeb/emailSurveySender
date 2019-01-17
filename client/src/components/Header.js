f15d1290fc1ecd51eedfb746d1cb3f590a4fde72
Juan Fox <foxj0875@gmail.com>
2019-01-16 22:25:13 -0500

    http-proxy-middleware package is installed and configured in client/src/setupProxy.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Sign in with Google</a></li>
+        );
+      default:
+        return <li><a href="">Welcome Rick !</a></li>;
+    }
+  }
   render() {
+    console.log(this.props.auth);
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="" className="brand-logo">Email Survey Sender</a>
           <ul className="right">
-            <li><a href="">Sign in with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +30,8 @@ class Header extends Component {
   }
 };
 
-export default Header;
+const mapStateToProps = ({ auth }) => {
+  return {auth};
+};
+
+export default connect(mapStateToProps)(Header);
