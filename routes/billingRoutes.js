4dc259c982ce838040c39360c87b5b8debd44bae
Juan Fox <foxj0875@gmail.com>
2019-01-22 13:09:09 -0500

    C:/Program Files/Git/api/stripe on the backend is adding credits to the logged in user model

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,18 @@
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
-    console.log(req.body);
-    res.send(req.user);
+  app.post('/api/stripe', async (req, res) => {
+    
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      source: req.body.id,
+      description: '5 credit purchase',
+    });
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
   });
 };
