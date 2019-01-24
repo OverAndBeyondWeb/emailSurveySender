6f9538bba472de6da75facc2a65b6a1960e216e2
Juan Fox <foxj0875@gmail.com>
2019-01-24 15:53:08 -0500

    Recipient model is coded

--- models/Survey.js
+++ models/Survey.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],
+  recipients: [RecipientSchema],
   yes: {
     type: Number,
     default: 0
@@ -14,7 +14,13 @@ const surveySchema = new Schema({
   no: {
     type: Number,
     default: 0
-  }
+  },
+  _user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  dateSent: Date,
+  lastResponded: Date
 });
 
 const Surveys = mongoose.model('surveys', surveySchema);
