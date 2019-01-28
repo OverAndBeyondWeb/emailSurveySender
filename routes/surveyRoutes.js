952862c0e0c6beba2197e10f00756ef113b3494c
Juan Fox <foxj0875@gmail.com>
2019-01-28 14:00:31 -0500

    sendgrid is recording clicks on links inside of sent emails

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,7 +8,11 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting');
+  });
+
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 
     const { title, subject, body, recipients } = req.body;
 
@@ -22,7 +26,16 @@ module.exports = app => {
     });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.sendEmail();
+    try {
+      await mailer.sendEmail();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch(err) {
+      res.status(422).send(err);
+    }
 
   });
   
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,8 +1,22 @@
+const keys = require('../../config/keys');
+
 module.exports = survey => {
   return `
-    <div>
-      Hello, how are you?
-      ${survey.body}
-    </div>
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3>I'd like your input!</h3>
+          <p>Please answer the following question:</p>
+          <p>${survey.body}</p>
+          <div>
+            <a href="${keys.redirectDomain}/surveys/thanks">Yes</a>
+          </div>
+          <div>
+            <a href="${keys.redirectDomain}">No</a>
+          </div>
+        </div>
+      </body>
+    </html>
+      
   `;
 };
