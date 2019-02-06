a685b811e18c4fb0f79f2947bcaa54f877b0c7fc
Juan Fox <foxj0875@gmail.com>
2019-02-06 12:59:09 -0500

    C:/Program Files/Git/api/surveys get route is providing user survey info from the db to the api

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,6 +10,11 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({_user: req.user.id});
+    res.send(surveys);
+  });
+
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting');
   });
